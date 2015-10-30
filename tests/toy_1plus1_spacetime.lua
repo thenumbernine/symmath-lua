60a43f637141476f9df2024dd421b0c558c43144
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-10-30 17:47:58 +0300

    got toy 1+1 running.  not good results, but it runs.

--- tests/toy_1plus1_spacetime.lua
+++ tests/toy_1plus1_spacetime.lua
@@ -12,6 +12,11 @@ symmath.tostring = MathJax
 
 print(MathJax.header)
 
+local function printbr(...)
+	print(...)
+	print'<br>'
+end
+
 local x = symmath.var'x'
 local t = symmath.var('h', {x})
 
@@ -24,155 +29,128 @@ Tensor.coords{
 }
 
 local r = Tensor('^I', t,x)
-print('\\(r^I = \\)',r'^I','<br>')
+printbr('$r^I = $',r'^I')
 
 local e = Tensor'_u^I'
 e['_u^I'] = r'^I_,u':simplify()
-print('\\({e_u}^I = \\)',e'_u^I','<br>')
+printbr('${e_u}^I = $',e'_u^I')
 
 local eta = symmath.Tensor('_IJ',{-1,0},{0,1})
-print('\\(\\eta = \\)',eta'_IJ','<br>')
+printbr('$\\eta = $',eta'_IJ')
 Tensor.metric(eta, eta, 'I')
 
 local g = (e'_u^I' * e'_v^J' * eta'_IJ'):simplify()
-print('\\(g_{uv} = \\)',g'_uv','<br>')
+printbr('$g_{uv} = $',g'_uv')
 
 -- [[ ...and out of nowhere, force betas to be zero
 g[1][2] = symmath.Constant(0)
 g[2][1] = symmath.Constant(0)
-print'zero skew components:<br>'
-print('\\(g_{uv} = \\)',g'_uv','<br>')
+printbr'zero skew components:'
+printbr('$g_{uv} = $',g'_uv')
 --]]
 
-print'set metric<br>'
-local gInv = Tensor.metric(g).matrixInverse
-print('\\(g^{uv} = \\)',g'^uv':simplify(),'<br>')
+printbr'set metric'
+Tensor.metric(g)
+printbr('$g^{uv} = $', g'^uv')
 
 -- [[ simply solve for any unit vector ...
 local nUt, nUx = symmath.vars('n^t', 'n^x')
 local n = Tensor('^u', nUt, nUx)
-print('\\(n^u = \\)',n'^u','<br>')
+printbr('$n^u = $',n'^u')
 
 -- TODO auto transform between coordinates using vielbein 
 local n_dot_e = (n'^u' * e'_u^I'):equals(Tensor'^I'):simplify()
 local n_dot_e_x = n_dot_e:lhs()[1]:equals(n_dot_e:rhs()[1])
-print('\\(n_I = \\)',n_dot_e,'<br>')
+printbr('$n_I = $',n_dot_e)
 
 local nUt_from_nUx = n_dot_e_x:solve(nUt)
-print('\\(n^t\\) from \\(n^x\\) : ',nUt_from_nUx,'<br>')
+printbr('$n^t$ from $n^x$ : ',nUt_from_nUx)
 
 local n_norm = (n'^u' * n'_u'):simplify():equals(-1)
-print('\\(n \\cdot n = -1\\) : ',n_norm,'<br>')
+printbr('$n \\cdot n = -1$ : ',n_norm)
 local n_norm_from_nUx = n_norm:subst(nUt_from_nUx):simplify()
-print(n_norm_from_nUx,'<br>')
+printbr(n_norm_from_nUx)
 local nUx_solns = table{n_norm_from_nUx:solve(nUx)}
-print('\\(n^x\\) solutions:<br>')
-print(nUx_solns:map(function(eqn) return '\t'..eqn end):concat('\n'),'<br>')
-print("solving the first one first (keep note that they're plus or minus...<br>")
+printbr'$n^x$ solutions:'
+printbr(nUx_solns:map(function(eqn) return '\t'..eqn end):concat('\n'))
+printbr"solving the first one first (keep note that they're plus or minus...)"
 local nUx_soln = nUx_solns[1]
-print('\\(n^x\\) is  ',nUx_soln:rhs(),'<br>')
+printbr('$n^x$ is  ',nUx_soln:rhs())
 -- (-d/{dx}[h] sqrt(4 + 8 (d/{dx}[h])^4 - 12 (d/{dx}[h])^2)) / (-2 - 4 (d/{dx}[h])^4 + 6 (d/{dx}[h])^2)
 local cmp = (-t:diff(x) * symmath.sqrt(4 + 8 * t:diff(x)^4 - 12 * t:diff(x)^2)) / (-2 - 4 * t:diff(x)^4 + 6 * t:diff(x)^2)
-print('should be',cmp,'<br>')
+printbr('should be',cmp)
 -- assert we're here.  I'm going to simplify this by hand since I don't have a good polynomial factoring algorithm ...
 --assert((nUx_soln:rhs():equals(cmp):isTrue())
 -- h' / sqrt((1 - h'^2)*(1 - 2 h'^2))
 local nUx_soln = nUx:equals(t:diff(x) / symmath.sqrt((1 - t:diff(x)^2)*(1 - 2*t:diff(x)^2)))
-print('rewritten by hand :',nUx_soln,'<br>')
+printbr('rewritten by hand :',nUx_soln)
 local nUt_soln = nUt_from_nUx:subst(nUx_soln):simplify()	-- also plus or minus, since n^t is scaled by n^x
-print('\\(n^t\\) solutions:<br>')
-print(nUt_soln,'<br>')
-print(nUt:equals((-nUt_soln:rhs()):simplify()),'<br>')
+printbr'$n^t$ solutions:'
+printbr(nUt_soln)
+printbr(nUt:equals((-nUt_soln:rhs()):simplify()))
 -- once again, polynomial simplification ...
 -- (-1 + (d/{dx}[h])^2) / (-sqrt(1 - 3 (d/{dx}[h])^2 + 2 (d/{dx}[h])^4))
 local cmp = (-1 + t:diff(x)^2) / (-symmath.sqrt(1 - 3 * t:diff(x)^2 + 2 * t:diff(x)^4))
-print('should be',cmp,'<br>')
+printbr('should be',cmp)
 --assert((nUt_soln:rhs():equals(cmp):isTrue())
 -- (1 - h'^2) / sqrt((1 - h'^2)*(1 - 2 h'^2))
 -- (1 - h'^2) / (sqrt(1 - h'^2)*sqrt(1 - 2 h'^2))
 local nUt_soln = nUt:equals(symmath.sqrt((1 - t:diff(x)^2) / (1 - 2 * t:diff(x)^2)))
-print(nUt_soln,'<br>')
+printbr(nUt_soln)
 
 local n = symmath.Tensor('^u', nUt_soln:rhs(), nUx_soln:rhs())
-print('\\(n^u = \\)',n,'<br>')
+printbr('$n^u = $',n)
 
 local n_dot_e_x = (n'^u' * e'_u^I'):simplify():equals(0)
-print('\\(n_\\hat{x} = 0\\)<br>')
-print(n_dot_e_x,'<br>')
+printbr('$n_\\hat{x} = 0$')
+printbr(n_dot_e_x)
 
-local n_norm = (nU:transpose() * gLL * nU):simplify()[1][1]:equals(-1)
-print('n dot n = -1')
-print(n_norm)
+local n_norm = (n'^u' * g'_uv' * n'^v'):simplify():equals(-1)
+printbr('n dot n = -1')
+printbr(n_norm)
 -- and once again, polynomial division would show this to be true ...
 
 -- connection coefficients
-local connLLL = symmath.Array(xs:map(function(xi,i) 
-	return xs:map(function(xj,j) 
-		return xs:map(function(xk,k) 
-			return ((gLL[i][j]:diff(xk) + gLL[i][k]:diff(xj) - gLL[j][k]:diff(xi))/2):simplify()
-		end)
-	end)
-end):unpack())
-print('conn_ijk = '..connLLL)
+local Gamma = ((g'_ab,c' + g'_ac,b' - g'_bc,a')/2):simplify()
+printbr('1st kind Christoffel')
+printbr('$\\Gamma_{abc} = $'..Gamma'_abc')
+printbr()
 
-local connULL = symmath.Array(xs:map(function(_,i) 
-	return xs:map(function(_,j) 
-		return xs:map(function(_,k) 
-			local sum = 0
-			for l=1,#xs do
-				sum = sum + gUU[i][l] * connLLL[l][j][k]
-			end
-			return sum:simplify()
-		end)
-	end)
-end):unpack())
-print('conn^i_jk = '..connULL)
+Gamma = Gamma'^a_bc':simplify()	-- change form stored in Gamma from 1st to 2nd kind
 
-local nL = (gLL * nU):simplify()
-print('n_u = '..nL)
+-- Christoffel: G^a_bc = g^ae G_ebc
+printbr('2nd kind Christoffel')
+printbr('${\\Gamma^a}_{bc} = $'..Gamma'^a_bc')
+printbr()
 
-local partialL_nL = symmath.Array(xs:map(function(xu,u)
-	return xs:map(function(xv,v)
-		return nL[v][1]:diff(xu):simplify()
-	end)
-end):unpack())
-print('partial_u n_v = '..partialL_nL)
+printbr('$n_u = $'..n'_u')
+printbr('$\\partial_u n_v = $'..n'_u,v')
 
-local diffL_nL = symmath.Array(xs:map(function(xu,u)
-	return xs:map(function(xv,v)
-		local sum = 0
-		for w,xw in ipairs(xs) do
-			sum = sum + connULL[w][v][u] * nL[w][1]
-		end
-		return (partialL_nL[u][v] - sum):simplify()
-	end)
-end):unpack())
-print('diff_u n_v = '..diffL_nL)
+local diff_n = (n'_u,v' - Gamma'^w_vu' * n'_w'):simplify()
+printbr('$\\nabla_u n_v = $'..diff_n'_uv')
 
-local deltaUL = (eta * eta):simplify()
-print('delta^u_v = '..deltaUL)
-
-local PUL = (deltaUL + nU * nL:transpose()):simplify()
+local P = (g'^u_v' + n'^u' * n'_v'):simplify()
 --[[ explicitly force extrinsic projection values to zero?
 PUL[1][2] = symmath.Constant(0)
 PUL[2][1] = symmath.Constant(0)
 --]]
-print('proj = P^u_v = '..PUL)
+printbr('$\\perp = {P^u}_v = $'..P'^u_v')
 
-local proj_diffL_nL = (PUL:transpose() * diffL_nL * PUL):simplify()
-print('proj diff_u n_v = '..proj_diffL_nL)
+local proj_diff_n = (P'^c_a' * diff_n'_cd' * P'^d_b'):simplify()
+printbr('$\\perp \\nabla_u n_v = $'..proj_diff_n)
 
-local KLL = (-(proj_diffL_nL + proj_diffL_nL:transpose())/2):simplify()
-print('K_uv = '..KLL)
+local K = (-(proj_diff_n'_ab' + proj_diff_n'_ba')/2):simplify()
+printbr('$K_{uv} = $'..K'_uv')
 
-local KLL = (-(diffL_nL + nL * nU:transpose() * diffL_nL)):simplify()
-print('K_uv = '..KLL)
+-- here's another example of where overloading tensor indexing on *all* expressions is handy ... what if we put a minus sign outside one?  and tidy() gives us unm(Tensor)?
+--local K = ((-diff_n'_ab' - n'_a' * n'^b' * diff_n'_ba')):simplify()
+--printbr('$K_{uv} = $'..K'_ab')	
 
-local KUL = (gUU * KLL):simplify()
-print('K^u_v = '..KUL)
+local K = K'^a_b'
+printbr('${K^u}_v = $'..K'^a_b')
 
-local K = (KUL[1][1] + KUL[2][2]):simplify()
-print('K = '..K)
+local K = K'^a_a':simplify()
+printbr('$K = $'..K)
 -- ... so where did K = -h'' / sqrt(g_xx) = -h'' / sqrt(1 - h'^2) come from?
 -- maybe with a different choice of n?
 
