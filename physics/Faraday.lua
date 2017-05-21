970fd0fc3b9b61cf777173243ddf1d9094c0a34b
Christopher Moore <christopher.e.moore@gmail.com>
2017-05-21 20:11:31 +0300

    another sign fix

--- physics/Faraday.lua
+++ physics/Faraday.lua
@@ -1,6 +1,7 @@
 --[[
 creates the Faraday tensor
 using equations 2.24 through 2.28 of "The Einstein-Maxwell system in 3+1 form and initial data for multiple charged black holes"
+and then tweaking signs to match "Gravitation" Misner Thorne & Wheeler eqn. 3.7
 args:
 	g = (optional) metric to use
 	gU = (optional) metric inverse
@@ -23,6 +24,6 @@ return function(args)
 	local E = args.E or Tensor('_a', function(a) return a == 1 and 0 or var('E_{'..basis.variables[a].name..'}') end)
 	local B = args.B or Tensor('_a', function(a) return a == 1 and 0 or var('B_{'..basis.variables[a].name..'}') end)
 	local F = Tensor'_ab'
-	F['_ab'] = (n'_e' * gU'^ed' * LeviCivita'_adbf' * gU'^fg' * B'_g' - n'_a' * E'_b' + E'_a' * n'_b')()
+	F['_ab'] = (E'_a' * n'_b' - n'_a' * E'_b' - n'_e' * gU'^ed' * LeviCivita'_abdf' * gU'^fg' * B'_g')()
 	return F
 end
