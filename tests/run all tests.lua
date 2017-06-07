ed48cb1c4aae0ef0b1ab55dc1ff20d2ef3772487
Christopher Moore <christopher.e.moore@gmail.com>
2017-06-07 13:34:04 +0300

    added timestamp verifications

--- tests/run all tests.lua
+++ tests/run all tests.lua
@@ -1,4 +1,5 @@
 #!/usr/bin/env luajit
+local lfs = require 'lfs'
 require 'ext'
 local function recurse(dir)
 	for f in file[dir]() do
@@ -6,9 +7,15 @@ local function recurse(dir)
 		if io.isdir(fn) then
 			recurse(fn)
 		elseif f:sub(-4) == '.lua' then
-			os.execute('mkdir output/'..dir)
-			os.execute(fn..' > output/'..fn:sub(1,-5)..'.html')
+			local target = 'output/'..fn:sub(1,-5)..'.html'
+			if lfs.attributes(target).change < lfs.attributes(fn).change then
+				os.execute('mkdir output/'..dir)
+				os.execute('"'..fn..'" > "'..target..'"')
+			else
+				print(fn..' is up-to-date.')
+			end
 		end
+		os.exit()
 	end
 end
 recurse'.'
