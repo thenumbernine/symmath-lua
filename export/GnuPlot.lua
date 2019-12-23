e9c0e4486147a155d8b1bd8cfadaa98978e19703
Christopher Moore <christopher.e.moore@gmail.com>
2019-12-23 16:08:22 +0600

    removed var list from gnuplot as it was already removed from other exporters

--- export/GnuPlot.lua
+++ export/GnuPlot.lua
@@ -5,50 +5,47 @@ local Language = require 'symmath.export.Language'
 local GnuPlot = class(Language)
 
 GnuPlot.lookupTable = {
-	[require 'symmath.Constant'] = function(self, expr, vars)
+	[require 'symmath.Constant'] = function(self, expr)
 		local s = tostring(expr.value)
 		if not s:find'.' and not s:find'[eE]' then s = s .. '.' end
 		return s
 	end,
-	[require 'symmath.Invalid'] = function(self, expr, vars)
+	[require 'symmath.Invalid'] = function(self, expr)
 		return '(0/0)'
 	end,
-	[require 'symmath.Function'] = function(self, expr, vars)
+	[require 'symmath.Function'] = function(self, expr)
 		return expr.name .. '(' .. table.map(expr, function(x,k)
 			if type(k) ~= 'number' then return end
-			return self:apply(x, vars)
+			return self:apply(x)
 		end):concat(',') .. ')'
 	end,
-	[require 'symmath.op.unm'] = function(self, expr, vars)
-		return '(-'..self:apply(expr[1], vars)..')'
+	[require 'symmath.op.unm'] = function(self, expr)
+		return '(-'..self:apply(expr[1])..')'
 	end,
-	[require 'symmath.op.Binary'] = function(self, expr, vars)
+	[require 'symmath.op.Binary'] = function(self, expr)
 		return '('..table.map(expr, function(x,k)
 			if type(k) ~= 'number' then return end
-			return self:apply(x, vars)
+			return self:apply(x)
 		end):concat(' '..expr.name..' ')..')'
 	end,
-	[require 'symmath.op.pow'] = function(self, expr, vars)
+	[require 'symmath.op.pow'] = function(self, expr)
 		if expr[1] == require 'symmath'.e then
-			return '(exp('..self:apply(expr[2], vars)..'))'
+			return '(exp('..self:apply(expr[2])..'))'
 		else
 			return '('..table.map(expr, function(x,k)
 				if type(k) ~= 'number' then return end
-				return self:apply(x, vars)
+				return self:apply(x)
 			end):concat(' ** ')..')'
 		end
 	end,
-	[require 'symmath.Variable'] = function(self, expr, vars)
-		if table.find(vars, nil, function(var) return expr.name == var.name end) then
-			return expr.name
-		end
-		error("tried to compile variable "..expr.name.." that wasn't in your function argument variable list")
+	[require 'symmath.Variable'] = function(self, expr)
+		return expr.name
 	end,
 	[require 'symmath.Derivative'] = function(self, expr) 
 		error("can't compile differentiation.  replace() your diff'd content first!")
 	end,
-	[require 'symmath.Heaviside'] = function(self, expr, vars)
-		return '('..self:apply(expr[1], vars)..' >= 0.)'
+	[require 'symmath.Heaviside'] = function(self, expr)
+		return '('..self:apply(expr[1])..' >= 0.)'
 	end,
 }
 
@@ -64,7 +61,7 @@ function GnuPlot:plot(args)
 		local expr = arg[1]
 		if Expression.is(expr) then
 			local x = arg.x or var'x' arg.x = nil
-			args[i][1] = self:apply(expr:replace(x, var'x'), {var'x'})
+			args[i][1] = self:apply(expr:replace(x, var'x'))
 		end
 	end
 
