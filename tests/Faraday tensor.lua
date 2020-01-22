5574d9c34c3ba178d61a25b4eff28a7f32b01ad6
Christopher Moore <christopher.e.moore@gmail.com>
2020-01-22 12:27:49 +0600

    even faster, don't use identity matrices

--- tests/Faraday tensor.lua
+++ tests/Faraday tensor.lua
@@ -42,36 +42,8 @@ end
 -- I bet it would be faster if we just used the rank-2 delta representation of the rank-2n delta
 local startTime = os.clock()
 io.stderr:write('starting d3^abc_def * partialFDef_abc...\n') io.stderr:flush()
---[[ it's taking 4:19 to finish
---local extFDef = (d3'^abc_def' * partialFDef'_abc')()
---local extFDef = (d3 * partialFDef)()
---]]
---[[ takes 5:13 to finish
-local extFDef = (
-	(
-		d1'^a_d' * d1'^b_e' * d1'^c_f' 
-		+ d1'^a_e' * d1'^b_f' * d1'^c_d' 
-		+ d1'^a_f' * d1'^b_d' * d1'^c_e' 
-		- d1'^a_f' * d1'^b_e' * d1'^c_d' 
-		- d1'^a_d' * d1'^b_f' * d1'^c_e' 
-		- d1'^a_e' * d1'^b_d' * d1'^c_f' 
-	) * partialFDef'_abc'
-)()
---]]
--- [[	-- 5.8 seconds
 local extFDef = KronecherDelta3(partialFDef)
---]]
-
 io.stderr:write('...done.  took '..(os.clock()-startTime)..' seconds\n') io.stderr:flush()
---[[
-local extFDef = Tensor('_abc', function(a,b,c)
-	local sum = 0
-	for all permutations of a,b,c do
-		sum = sum + parity * partialFDef[a][b][c]
-	end
-	return sum()
-end)
---]]
 printbr'verify $d^2 A = 0$'
 printbr(var'dF''_abc':eq(extFDef))
 
@@ -79,7 +51,7 @@ FDef = Tensor('_ab', function(a,b)
 	if a==b then return 0 end
 	local s = 1
 	if a>b then a,b,s = b,a,-1 end
-	return s * var('F_{'..txs[a].name..txs[b].name..'}', txs)
+	return (s * var('F_{'..txs[a].name..txs[b].name..'}', txs))()
 end)
 printbr(F'_ab':eq(FDef))
 
@@ -99,6 +71,6 @@ local extFDef = Tensor('_abc', function(a,b,c)
 	return sum()
 end)
 --]]
-printbr'$d^2 A = 0$ in terms of $F_{ab}'
+printbr'$d^2 A = 0$ in terms of $F_{ab}$'
 printbr(var'dF''_abc':eq(extFDef))
 
