4e11ad5e804986d22bfdba998cf421c7e1d657cd
Christopher Moore <christopher.e.moore@gmail.com>
2020-04-03 11:17:04 +0600

    not long until I just merge this with my metric catalog

--- tests/Parallel Propagators.lua
+++ tests/Parallel Propagators.lua
@@ -13,72 +13,69 @@ for _,info in ipairs{
 	{
 		name = 'polar, coordinate',
 		coords = {r, phi},
-		conns = {
-			Matrix.diagonal(0, 1/r),
-			Matrix({0, -r}, {1/r, 0}),
-		},
+		getConn = function()
+			local conn = Tensor'^a_bc'
+			conn[2][1][2] = 1/r
+			conn[1][2][2] = -r
+			conn[2][2][1] = 1/r
+			return conn
+		end,
 	},
 	{
 		name = 'polar, orthonormal',
 		coords = {r, phi},
-		conns = {
-			Matrix.diagonal(0, 0),			-- Gamma_rHat
-			--Matrix({0, -1/r}, {1/r, 0})	-- Gamma_phiHat
-			Matrix({0, -1}, {1, 0}),		-- Gamma_phi = e_phi^phiHat Gamma_phiHat = r Gamma_phiHat
-		},
+		getConn = function()
+			local conn = Tensor'^I_JK'
+			conn[1][2][2] = -1/r
+			conn[2][2][1] = 1/r
+			return conn
+		end,
+		-- e_a = e_a^I e_I for orthonormal basis e_I and coord derivative e_a
+		-- s.t. g_ab = e_a^I e_b^J eta_IJ
+		getLinCoeff = function()
+			return Tensor('_a^I',
+				{1, 0},
+				{0, r}
+			)
+		end,
 	},
 	{
 		name = 'spherical, coordinate',
 		coords = {r, theta, phi},
-		conns = {
-			Matrix.diagonal(0, 1/r, 1/r),
-			Matrix(
-				{0, -r, 0}, 
-				{1/r, 0, 0}, 
-				{0, 0, cos(theta)/sin(theta)}
-			),
-			Matrix(
-				{0, 0, -r*sin(theta)^2}, 
-				{0, 0, -sin(theta)*cos(theta)}, 
-				{1/r, cos(theta)/sin(theta), 0}
-			),
-		},
+		getConn = function()
+			local conn = Tensor'^a_bc'
+			conn[2][1][2] = 1/r
+			conn[3][1][3] = 1/r
+			conn[1][2][2] = -r
+			conn[2][2][1] = 1/r
+			conn[3][2][3] = cos(theta) / sin(theta)
+			conn[1][3][3] = -r*sin(theta)^2
+			conn[2][3][3] = -sin(theta)*cos(theta)
+			conn[3][3][1] = 1/r
+			conn[3][3][2] = cos(theta)/sin(theta)
+			return conn
+		end,
 	},
 	{
 		name = 'spherical, orthonormal',
 		coords = {r, theta, phi},
-		conns = {
-			-- Gamma_rHat
-			Matrix.diagonal(0, 0, 0),
-			--[[ Gamma_thetaHat:
-			Matrix(
-				{0, -1/r, 0},
-				{1/r, 0, 0},
-				{0, 0, 0}
+		getConn = function()
+			local conn = Tensor'^a_bc'
+			conn[1][2][2] = -1/r
+			conn[2][2][1] = 1/r
+			conn[1][3][3] = -1/r
+			conn[2][3][3] = -cos(theta)/(r*sin(theta))
+			conn[3][3][1] = 1/r
+			conn[3][3][2] = cos(theta)/(r*sin(theta))
+			return conn
+		end,
+		getLinCoeff = function()
+			return Tensor('_a^I',
+				{1, 0, 0},
+				{0, r, 0},
+				{0, 0, r*sin(theta)}
 			)
-			--]]
-			-- [[ Gamma_theta = e_theta^thetaHat Gamma_thetaHat = r Gamma_thetaHat 
-			Matrix(
-				{0, -1, 0},
-				{1, 0, 0}, 
-				{0, 0, 0}
-			),
-			--]]
-			--[[ Gamma_phiHat
-			Matrix(
-				{0, 0, -1/r}, 
-				{0, 0, -cos(theta)/(r*sin(theta))}, 
-				{1/r, cos(theta)/(r*sin(theta)), 0}
-			),
-			--]]
-			-- [[ Gamma_phi = e_phi^phiHat Gamma_phiHat = r sin(theta) Gamma_phiHat
-			Matrix(
-				{0, 0, -sin(theta)}, 
-				{0, 0, -cos(theta)}, 
-				{sin(theta), cos(theta), 0}
-			),
-			--]]
-		},
+		end,
 	},
 
 } do
@@ -86,9 +83,32 @@ for _,info in ipairs{
 	printbr('<h3>'..info.name..':</h3>')
 
 	local coords = info.coords
-	local conns = info.conns
+	Tensor.coords{
+		{variables=coords},
+	}
+
+	printbr('coordinates:'..table.mapi(coords, tostring):concat', ')
+	printbr()
+
+	local connT = info.getConn()
 	local n = #coords
-	assert(n == #conns)
+	assert(n == #connT)
+
+	print('connection:')
+	connT:printElem'\\Gamma'
+	printbr()
+
+	-- connection: Gamma^I_JK, except rescale the lower term to a coordinate basis:
+	-- Gamma^I_aK = Gamma^I_JK * e_a^J
+	local connCoord = connT
+	if info.getLinCoeff then
+		local e = info.getLinCoeff() 
+		connCoord = (connT'^I_JK' * e'_a^J')():permute'^I_aK'
+		
+		print('connection with 2nd term transformed to a coordinate basis:')
+		connCoord:printElem'\\bar{\\Gamma}'
+		printbr()
+	end
 
 	printbr'parallel propagators:'
 	printbr()
@@ -99,7 +119,10 @@ for _,info in ipairs{
 	local P = var'P'
 
 	for i, coord in ipairs(coords) do
-		local conn = conns[i]
+		local conn = Matrix:lambda({n,n}, function(a,b)
+			return connCoord[a][i][b]
+		end)
+		--local conn = conns[i]
 		local name = coords[i].name	
 		
 		printbr(var('[\\Gamma_'..name..']'):eq(conn))
