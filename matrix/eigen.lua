1220d008a7b99f7ebd948a0e3cce5fc817d9c256
Christopher Moore <christopher.e.moore@gmail.com>
2020-04-23 00:05:20 +0600

    added 'defective' flag to eigensystem decomposition

--- matrix/eigen.lua
+++ matrix/eigen.lua
@@ -1,5 +1,21 @@
 local table = require 'ext.table'
 
+--[[
+A = square matrix
+
+returns: table with the following fields set:
+	lambdas = table of the following entries:
+		expr = expression of the eigenvalue
+		mult = multiplicity of the eigenvalue
+	defective = true if the matrix is defective (i.e. if an eigenvalue multiplicity is greater than the eigenvector dimension / (A - lambda I)-nullspace dimension )
+	
+(TODO maybe don't return the rest of these, 
+but provide functions for producing them?)
+	allLambdas = table of eigenvalues, with duplicates equal to the eigenvalue multiplicity, in the matching order of 'lambdas' above.
+	Lambda = diagonal matrix of the eigenvalues.  equivalent of Matrix.diag(allLambdas:unpack())
+	R = right eigenvectors, matching 1-1 with allLambdas
+	L = left eigenvectors.  produced by R:inverse()  these don't exist if A is defective.
+--]]
 local function eigen(A)
 	local symmath = require 'symmath'
 	local Matrix = symmath.Matrix
@@ -29,16 +45,18 @@ local function eigen(A)
 --for _,info in ipairs(lambdas) do
 --	printbr('mult '..info.mult..' expr '..info.expr)
 --end
-	
+
+	local defective
 	allLambdas = table()	-- redo allLambdas so the order matches the right-eigenvectors' order
 	local Rs = lambdas:mapi(function(lambdaInfo) 
 		local lambda = lambdaInfo.expr
 		local Ri = (A - lambda * I)():nullspace()
 		local n = Ri and #Ri[1] or 0
 		-- ex: A[1][2] = 1 otherwise A[i][j] = 0.  charpoly is lambda=0 mult4, nullspace is dim=3
-		--if lambdaInfo.mult ~= n then
-		--	error("nullspace of "..lambda.." is "..n.." but multiplicity of eigenvalue is "..lambdaInfo.mult)
-		--end
+		if lambdaInfo.mult ~= n then
+			defective = true
+			--error("nullspace of "..lambda.." is "..n.." but multiplicity of eigenvalue is "..lambdaInfo.mult)
+		end
 		--for i=1,lambdaInfo.mult do
 		for i=1,n do
 			allLambdas:insert(lambdaInfo.expr)
@@ -57,7 +75,7 @@ local function eigen(A)
 	-- inverse() isn't possible if R isn't square ... which happens when the charpoly mult != the nullspace dim
 	-- in that case, use SVD?
 	-- or solve manually for left-eigenvectors?
-	local L = R:inverse()
+	local L = not defective and R:inverse() or nil
 --printbr(var'L':eq(L))
 	local Lambda = Matrix.diagonal( allLambdas:unpack() )
 --printbr(var'\\Lambda':eq(Lambda))
@@ -76,6 +94,8 @@ local function eigen(A)
 		Lambda = Lambda,
 		R = R,
 		L = L,
+	
+		defective = defective,
 	}
 end
 
