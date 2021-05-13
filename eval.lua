5ff3dc183d423f4826757cb4960d188057a63368
Christopher Moore <christopher.e.moore@gmail.com>
2021-05-13 12:16:53 +0700

    rewrote eval() to use Lua compiling, so no more multiple pathways to the same thing, and compile() is more flexible

--- eval.lua
+++ eval.lua
@@ -1,1 +1,24 @@
-return require 'symmath.visitor.Eval'()
+--[[
+numerically evaluate the expression
+vars = {
+	{[variable/name] = [value/constant]},
+	...
+}
+--]]
+return function(expr, vars)
+	local symmath = require 'symmath'
+
+	for k,v in pairs(vars) do
+		local k = k
+		if type(k) == 'string' then k = var(k) end
+		-- clone so we can handle numbers and variable constants
+		expr = expr:replace(k, symmath.clone(v))
+	end
+	
+	-- final simplify in case a variable constant was substituted,
+	--  and we have a simplify() rule for it
+	expr = expr()
+
+	local f = expr:compile()
+	return f()
+end
