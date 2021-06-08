b5986f6929b07a4419f3bed7afb21fae0176bfaa
Christopher Moore <christopher.e.moore@gmail.com>
2021-06-08 15:52:18 +0600

    typo

--- hasChild.lua
+++ hasChild.lua
@@ -2,7 +2,7 @@
 example: 
 expr:hasChild(function(x) return whatever:isa(x) end)
 --]]
-local function hasChild(expr, lookfor)
+local function hasChild(x, lookfor)
 	if lookfor(x) then return true end
 	for i=1,#x do
 		if hasChild(x[i], lookfor) then return true end
