bf4e35f1254a216b601c23bda5500ebef74e9ef2
Christopher Moore <christopher.e.moore@gmail.com>
2022-06-22 16:16:04 +0600

    more

--- tests/Schwarzschild - spherical.lua
+++ tests/Schwarzschild - spherical.lua
@@ -10,16 +10,19 @@ require 'symmath'.setup{
 }
 
 -- coordinates
-local t,r,theta,phi = vars('t','r','\\theta','\\phi')
+local t,theta,phi = vars('t','\\theta','\\phi')
+local r = set.nonNegativeReal:var'r'
 
 -- mass
 local R_of_r = false
 -- dependent on r or not?  most derivations treat R as constant, but for stellar models R varies inside of the star
 -- TODO to match up with MTW, use 'R' for the planet radius and 'M' for the total mass, so 2 M for the Schwarzschild radius
-local R = var('R', R_of_r and {r} or nil)	
+local R = set.nonNegativeReal:var('R', R_of_r and {r} or nil)	
 
 local coords = {t,r,theta,phi}
-local chart = Tensor.Chart{coords=coords}
+Tensor.Chart{coords=coords}
+Tensor.Chart{coords={t}, symbols='t'}
+Tensor.Chart{coords={r,theta,phi}, symbols='ijklmn'}
 
 -- schwarzschild metric in cartesian coordinates
 local g = Tensor('_uv', function(u,v) return u == v and ({-(1-R/r), 1/(1-R/r), r^2, r^2 * symmath.sin(theta)^2})[u] or 0 end) 
@@ -45,5 +48,47 @@ end)
 -- TODO unravel equaliy, or print individual assignments
 printbr(d2x'^a':eq(-Gamma'^a_bc' * dx'^b' * dx'^c')())
 printbr()
+
+printbr'ADM variables:'
+local gVar = var'g'
+local alpha, beta, gamma = vars('\\alpha', '\\beta', '\\gamma')
+local alphaDef = alpha:eq(sqrt(-gVar'^tt'))
+local betaDef = beta'_i':eq(gVar'_ti')
+local gammaDef = gamma'_ij':eq(gVar'_ij')
+printbr(alphaDef)
+printbr(betaDef)
+printbr(gammaDef)
+printbr()
+
+local gU = props.gU
+
+local alphaVal = (1/sqrt(-gU'^tt'))()
+printbr(alpha:eq(alphaVal))
+
+local gammaVal = g'_ij'()
+printbr(gamma'_ij':eq(gammaVal))
+local gammaUVal = Tensor('^ij', gammaVal:inverse():unpack())
+printbr(gamma'^ij':eq(gammaUVal))
+
+local betaLVal = g'_ti'()
+printbr(beta'_i':eq(betaLVal))
+local betaVal = (gammaUVal'^ij' * betaLVal'_j')()
+printbr(beta'^i':eq(betaVal))
+
+local n = var'n'
+local nVal = Tensor('_a', -alphaVal, 0, 0, 0)
+printbr(n'_a':eq(nVal))
+printbr(n'^a':eq((gU'^ab' * nVal'_b')()))
+
+printbr((n'_a' * n'^a'):eq(nVal'_a' * nVal'^a')())
+printbr((n'_a' * n'_b' * gVar'^ab'):eq(nVal'_a' * nVal'_b' * gU'^ab')())
+
+local deltaVal = Tensor('_a^b', function(a,b) return a==b and 1 or 0 end)
+local gammaULVal = (deltaVal'_a^b' + nVal'_a' * nVal'^b')()
+printbr(gamma'_a^b':eq(gammaULVal))
+
+local K = var'K'
+local KVal = (gammaULVal'_a^c' * gammaULVal'_b^d' * (nVal'_d,c' - Gamma'^e_cd' * nVal'_e'))()
+printbr(K'_ab':eq(KVal))
 
 print(symmath.export.MathJax.footer)
