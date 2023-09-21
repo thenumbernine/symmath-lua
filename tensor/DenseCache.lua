4322f0997e020035a8798603419437f0285dc58a
Christopher Moore <christopher.e.moore@gmail.com>
2023-09-21 17:10:54 +0700

    made it so adding to a dense-tensor-cache removes old matching-valence entries

--- tensor/DenseCache.lua
+++ tensor/DenseCache.lua
@@ -13,17 +13,19 @@ function DenseCache:init()
 	self.cache = table()
 end
 
--- for Tensor.Ref x, returns associated Tensor
-function DenseCache:get(x)
+-- returns the index of the cache with TensorRef x
+function DenseCache:find(x)
 	local Tensor = require 'symmath.Tensor'
 	local Variable = require 'symmath.Variable'
-
 	assert(Tensor.Ref:isa(x) and Variable:isa(x[1]))
-
---printbr('dense tensor cache has', cachedDenseTensors:mapi(function(t) return t[1] end):mapi(tostring):concat';')
-	local _, t = self.cache:find(nil, function(t)
+	return self.cache:find(nil, function(t)
 		return Tensor.Ref.matchesIndexForm(t[1], x)
 	end)
+end
+
+-- for Tensor.Ref x, returns associated Tensor
+function DenseCache:get(x)
+	local _, t = self:find(x)
 	if not t then return end
 	return t[2], t[1]
 end
@@ -32,6 +34,13 @@ end
 add an entry for Tensor.Ref 'x' and Tensor 'dense'
 --]]
 function DenseCache:add(x, dense)
+	-- remove old entries with matching valence
+	while true do
+		local i = self:find(x)
+		if not i then break end
+		self.cache:remove(i)
+	end
+	-- add new entry
 	self.cache:insert{x:clone(), dense}
 end
 
