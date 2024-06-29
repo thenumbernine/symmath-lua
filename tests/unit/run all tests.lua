ab3fbf1edc704769af74030d612357ad7bbe8a90
thenumbernine <christopher.e.moore@gmail.com>
2024-06-29 12:39:13 +0600

    made tests run in order, allowed cmdline lua= to override lua exec

--- tests/unit/run all tests.lua
+++ tests/unit/run all tests.lua
@@ -4,7 +4,7 @@ local force = cmdline.force
 
 -- vanilla lua on windows isn't processing escape codes for color changes etc, but luajit is.
 -- linux works fine for both of course
-local lua = 'luajit'
+local lua = cmdline.lua or 'luajit'
 
 --[[
 ex: to run all tests with the 'useHasBeenFlags' disabled:  ./run\ all\ tests.lua force "-e=require'symmath'.useHasBeenFlags=false"
@@ -14,22 +14,26 @@ if dashE then
 	lua = lua..' -e "'..dashE..'"'
 end
 
+local fs = table()
 for f in path:dir() do
-	f = f.path
-	if f:sub(-4) == '.lua'
-	and f ~= 'run all tests.lua'
-	and f ~= 'unit.lua'
+	fs:insert(f)
+end
+fs:sort(function(a,b) return a.path < b.path end)
+for _,f in ipairs(fs) do
+	if f.path:sub(-4) == '.lua'
+	and f.path ~= 'run all tests.lua'
+	and f.path ~= 'unit.lua'
 	-- the following aren't unit test suites
 	-- usually they have no asserts at all
 	-- (except plot or export which have no asserts but still serve a purpose for output of examples)
 	-- maybe some I should redo these into test suits
-	and f ~= 'determinant_performance.lua'
-	and f ~= 'linear solver.lua'
-	and f ~= 'sub-tensor assignment.lua'
-	and f ~= 'tensor use case.lua'
+	and f.path ~= 'determinant_performance.lua'
+	and f.path ~= 'linear solver.lua'
+	and f.path ~= 'sub-tensor assignment.lua'
+	and f.path ~= 'tensor use case.lua'
 	then
-		local target = '../output/unit/'..f:sub(1,-5)..'.html'
-		local fileattr = path(f):attr()
+		local target = '../output/unit/'..f.path:sub(1,-5)..'.html'
+		local fileattr = f:attr()
 		local targetattr = path(target):attr()
 		if fileattr and targetattr then
 			print('comparing '..os.date(nil, targetattr.change)..' vs '..os.date(nil, fileattr.change))
